e50244b5aec3f7e34148b9567bf5ffdd6f9f2b59
mehmetozbek <mozbek@hotmail.com>
2023-08-13 15:16:14 +0200

    2.Commit

--- src/A02.java
+++ src/A02.java
@@ -1,5 +1,7 @@
 public class A02 {
     public static void main(String[] args) {
         System.out.println("2.Class açıldı");
+        System.out.println("Committen sonra koda bir şeyler " +
+                "eklerseniz Proje ağacında Class ismi mavi olur");
     }
 }
